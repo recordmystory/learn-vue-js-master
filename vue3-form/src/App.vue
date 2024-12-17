637db06b2d1bd75ddc9b109b9c58549d5e9d0fcc
recordmystory <113417749+recordmystory@users.noreply.github.com>
2024-12-17 20:35:17 +0900

    add : vue3-form project
    - 간단한 로그인화면 만들기
    - axios, JSONPlaceholder 사용

--- vue3-form/src/App.vue
+++ vue3-form/src/App.vue
@@ -1,65 +1,53 @@
-<template>
-  <form action="" @submit.prevent="submitForm">
+<!-- 
+ 
+[ 간단한 로그인 화면 만들기 ]
+ - 아이디와 패스워드 입력받기
+ - 로그인 버트 클릭시 서버로 요청 보내기
+
+ v-model : 사용 예시) input box에 있는 데이터를 그대로 vue의 데이터에 엮고싶을 때 사용할 수 있음
+ -->
+ 
+ <template>
+  <form action="" @submit.prevent="submitForm"> <!-- 이벤트 기본동작 막기 -->
     <div>
-      <label for="userId">ID:</label>
-      <input id="userId" type="text" v-model="username">
+      <label for="userId">ID : </label>
+      <input id="userId" type="text" v-model="username"> <!-- v-model: 인풋에 들어간 데이터를 밑에 username과 연결 -->
     </div>
     <div>
-      <label for="password">PW:</label>
-      <input type="text" v-model="password">
+      <label for="password">PW : </label>
+      <input id="password" type="password" v-model="password">
     </div>
     <button type="submit">로그인</button>
   </form>
 </template>
 
 <script>
-import axios from 'axios'
-import { ref } from 'vue';
+  import axios from 'axios'
 
   export default {
-    setup() {
-      // data
-      var username = ref('');
-      var password = ref('');
-
-      // methods
-      var submitForm = () => {
-        axios.post('https://jsonplaceholder.typicode.com/users/', {
-          username: username.value,
-          password: password.value
-        }).then(response => {
-          console.log(response);
-        })
+    data() {
+      return {
+        username: '',
+        password: ''
       }
-      
-      return { username, password, submitForm }
     },
-    // methods: {
-    //   logText() {
-    //     this.pas
-    //   }
-    // }
-
-    // data() {
-    //   return {
-    //     username: '',
-    //     password: '',
-    //   }
-    // },
-    // methods: {
-    //   submitForm() {
-    //     // event.preventDefault();
-    //     const data = {
-    //       username: this.username,
-    //       password: this.password,
-    //     }
-    //     axios.post('https://jsonplaceholder.typicode.com/users/', data)
-    //       .then(response => {
-    //         console.log(response)
-    //       });
-    //     // console.log('제출됨')
-    //   }
-    // }
+    methods: {
+      submitForm(/** evt **/) { // form 태그내에 사용한 디렉티브로 인해 해당 이벤트를 받을 수 있음
+        // evt.preventDefault();
+        
+        const data = { username: this.username, password: this.password }; // Vue의 데이터 
+        axios.post('https://jsonplaceholder.typicode.com/users'/**  {
+          // username: this.username,
+          // password: this.password
+          
+        } **/, data)
+        .then(response => {
+          console.log(response);
+        });
+        
+        // console.log('제출');
+      }
+    }
   }
 </script>
 
